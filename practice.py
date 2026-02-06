ac29c2d0823e560c4bbcb9079eefe18388080e98
Harshit12450 <Hustlerharsshit12450@gmail.com>
2026-02-07 01:58:32 +0530

    Practice Questions

--- practice.py
+++ practice.py
@@ -57,58 +57,84 @@
 # print(unique_chr)
 # print(len(unique_chr))
  
-students={}
+# students={}
 
-while True:
-    print("\n Menu")
-    print("A. Add a Student")
-    print("B. Update Marks")
-    print("C.Search for a student")
-    print("D.Display all student and marks")
-    print("E. Program exited")
+# while True:
+#     print("\n Menu")
+#     print("A. Add a Student")
+#     print("B. Update Marks")
+#     print("C.Search for a student")
+#     print("D.Display all student and marks")
+#     print("E. Program exited")
 
-    user_choice=input("Enter the choice ").lower()
-    if user_choice== "a":
-        name=input("Enter the students name").lower()
-        marks=int(input("Enter the marks"))
-        students[name]=marks
-        print("Student added successfully")
+#     user_choice=input("Enter the choice ").lower()
+    # if user_choice== "a":
+    #     name=input("Enter the students name").lower()
+    #     marks=int(input("Enter the marks"))
+    #     students[name]=marks
+    #     print("Student added successfully")
 
     
-    elif user_choice == "b":
-        name = input("Enter student name to update: ").lower()
-        if name in students:
-            marks = int(input("Enter new marks: "))
-            students[name] = marks
-            print("Marks updated successfully.")
-        else:
-            print("Student not found.")
+    # elif user_choice == "b":
+    #     name = input("Enter student name to update: ").lower()
+    #     if name in students:
+    #         marks = int(input("Enter new marks: "))
+    #         students[name] = marks
+    #         print("Marks updated successfully.")
+    #     else:
+    #         print("Student not found.")
 
-    elif user_choice== "c":
-        name=input("Enter the name of student you want to search").lower()
-        if name in students:
-            print(f"{name} scored {students[name]} marks")  
-        else:
-            print("Student not found")
+    # elif user_choice== "c":
+    #     name=input("Enter the name of student you want to search").lower()
+    #     if name in students:
+    #         print(f"{name} scored {students[name]} marks")  
+    #     else:
+    #         print("Student not found")
 
-    elif user_choice=="d":
-        if students:
-           for name,marks in students.items():
-               print(name,marks)     
+    # elif user_choice=="d":
+    #     if students:
+    #        for name,marks in students.items():
+    #            print(name,marks)     
 
-        else:
-            print("No student records found")  
+    #     else:
+    #         print("No student records found")  
 
-    elif user_choice=="e":
-        print("program exited")
-        break
-    else:
-        print("invalid choice")                      
+    # elif user_choice=="e":
+    #     print("program exited")
+    #     break
+    # else:
+    #     print("invalid choice")                      
    
 
 # finally it worked
 
+class Product:
+    count=0
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+        
+        Product.count+=1
 
+    def get_info(self):
+        print(f"Tour product name is {self.name} and its price is {self.price}")
+    
+    @classmethod
+    def get_count(cls):
+        print(f"Your count will be {Product.count}")
+
+    def discount_price(self,price,discount):
+        print(f"Your discounted price will be {price - (price*discount/100)}")
+
+
+p1=Product("Lapto",78000)
+p2=Product("Pen",20)
+p3=Product("Phone",20000)
+
+p1.get_info()
+p1.get_count()
+p1.discount_price(20000,10)
+        
 
 
 
